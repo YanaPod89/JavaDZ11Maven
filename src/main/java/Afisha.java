2608cc19fb20bbd867142ff057eaec3b41ada9ce
Yana <podolskaya_22@mail.ru>
2024-09-04 13:13:43 +0700

    addTest

---
+++ src/main/java/Afisha.java
@@ -0,0 +1,42 @@
+public class Afisha {
+    private String[] film = new String[0];
+    private int limit = 5;
+
+    public Afisha() {
+    }
+
+    public Afisha(int limit) {
+        this.limit = limit;
+
+    }
+
+    public void addFilm(String newFilm) {
+        String[] tmp = new String[film.length + 1];
+        for (int i = 0; i < film.length; i++) {
+            tmp[i] = film[i];
+        }
+        tmp[tmp.length - 1] = newFilm;
+        film = tmp;
+    }
+
+    public String[] findAll() {
+        return film;
+    }
+
+    public String[] findLast() {
+
+        int resultLength;
+        if (limit < film.length) {
+            resultLength = limit;
+        } else {
+            resultLength = film.length;
+        }
+
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = film[film.length - 1 - i];
+        }
+        return tmp;
+
+    }
+}
---
+++ src/test/java/AfishaTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AfishaTest {
+    String film1 = "Бладшот";
+    String film2 = "Вперед";
+    String film3 = "Отель Белград";
+    String film4 = "Джентельмены";
+    String film5 = "Человек Неведимка";
+    String film6 = "Тролли";
+    String film7 = "Номер один";
+
+    @Test
+    public void shouldFindAll() {
+
+        Afisha star = new Afisha();
+
+        String[] actual = star.findAll();
+        String[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddFilm() {
+
+        Afisha star = new Afisha();
+        star.addFilm(film1);
+        star.addFilm(film2);
+        star.addFilm(film3);
+        star.addFilm(film4);
+        star.addFilm(film5);
+        String[] actual = star.findAll();
+        String[] expected = {film1, film2, film3, film4, film5};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLast() {
+
+        Afisha star = new Afisha(6);
+        star.addFilm(film1);
+        star.addFilm(film2);
+        star.addFilm(film3);
+        String[] actual = star.findLast();
+        String[] expected = {film3, film2, film1};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
